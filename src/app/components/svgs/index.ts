2072cc65c2bf708414cf85f57631b38bb9868d10
Florien Niyongabo <waka@192.168.1.76>
2025-01-25 23:06:14 +0200

    Configure @svgr/webpack

---
+++ src/app/components/svgs/index.ts
@@ -0,0 +1,8 @@
+export { default as DashLogo } from "./dash_logo.svg";
+export { default as Home } from "./home.svg";
+export { default as File } from "./file.svg";
+export { default as UserPlus } from "./user_plus.svg";
+export { default as Settings } from "./settings.svg";
+export { default as HelpCenter } from "./headset.svg";
+export { default as Gift } from "./gift.svg";
+export { default as Signout } from "./signout.svg";
