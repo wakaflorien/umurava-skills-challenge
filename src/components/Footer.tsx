8db0a8526091f62021af318f7e2ebdc34121d500
Florien Niyongabo <waka.florien45@gmail.com>
2025-02-05 11:32:55 +0200

    Refactor Footer component for improved layout and spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { BackToTop } from "./BackToTop";
 
 export const Footer = () => {
     return (
-        <footer className="bg-secondary flex flex-col gap-4 px-8 sm:px-24 py-2 sm:py-12 sm:divide-y divide-white/20">
-            <div className="flex items-center justify-between sm:py-8">
+        <footer className="bg-secondary flex flex-col gap-2 sm:gap-4 px-4 sm:px-24 py-8 sm:py-12 sm:divide-y divide-white/20">
+            <div className="flex items-center justify-between py-4 sm:py-8">
                 <Image
                     aria-hidden
                     src="/CareerTicket_4x-removebg-preview 1.png"
@@ -27,10 +27,10 @@ export const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-4  sm:flex-row items-start justify-between sm:py-8">
-                <div className="flex flex-col">
-                    <h1 className="text-white text-md sm:text-lg font-semibold">Our Address</h1>
-                    <ul className="flex flex-col sm:gap-2 sm:py-2 text-tertiary">
+            <div className="flex flex-col gap-8 sm:gap-4  sm:flex-row items-start justify-between py-4 sm:py-8">
+                <div className="flex flex-col gap-2 sm:gap-0">
+                    <h1 className="text-white text-sm sm:text-lg font-semibold">Our Address</h1>
+                    <ul className="flex flex-col gap-2 sm:gap-2 sm:py-2 text-tertiary">
                         <li className="flex gap-2">
                             <Image
                                 aria-hidden
@@ -41,7 +41,7 @@ export const Footer = () => {
                                 // layout="responsive"
                                 className={`object-cover`}
                             />
-                            <span>career@tickets.com</span>
+                            <p>career@tickets.com</p>
                         </li>
                         <li className="flex gap-2">
                             <Image
@@ -53,7 +53,7 @@ export const Footer = () => {
                                 // layout="responsive"
                                 className={`object-cover`}
                             />
-                            <span>89 KG 14 Ave, Kigali</span>
+                            <p>89 KG 14 Ave, Kigali</p>
                         </li>
                         <li className="flex gap-2">
                             <Image
@@ -65,31 +65,31 @@ export const Footer = () => {
                                 // layout="responsive"
                                 className={`object-cover`}
                             />
-                            <span>+250 700 000</span>
+                            <p>+250 700 000</p>
                         </li>
                     </ul>
                 </div>
-                <div className="flex flex-col">
-                    <h1 className="text-white text-md sm:text-lg font-semibold">Quick Links</h1>
-                    <ul className="flex flex-col sm:gap-2 sm:py-2 text-tertiary">
+                <div className="flex flex-col gap-2 sm:gap-0">
+                    <h1 className="text-white text-sm sm:text-lg font-semibold">Quick Links</h1>
+                    <ul className="flex flex-col gap-1 sm:gap-2 py-1 sm:py-2 text-tertiary">
                         <li><a href="./">Home</a></li>
-                        <li><a href="/program">Program</a></li>
+                        <li><a href="./#contact">Program</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="./#contact">Contact Us</a></li>
                     </ul>
                 </div>
-                <div className="flex flex-col gap-4 sm:gap-4">
-                    <h1 className="text-white text-md sm:text-lg font-semibold">Join our newsletter to keep up to date with us!</h1>
-                    <form className="flex items-center justify-between bg-white p-2 rounded-md">
-                        <input className="w-full text-black outline-none sm:p-3" placeholder="Email" />
+                <div className="flex flex-col gap-4 ">
+                    <h1 className="text-white text-sm sm:text-lg font-semibold">Join our newsletter to keep up to date with us!</h1>
+                    <form className="flex items-center justify-between bg-white p-1 sm:p-2 rounded-md">
+                        <input className="w-full text-black outline-none p-2 sm:p-3" placeholder="Email" />
                         <Button classNames="bg-primary text-white hover:bg-primary/90 font-semibold p-2 sm:p-3" label="Subscribe" onClick={() => console.log("Subscribed")} />
                     </form>
 
                 </div>
             </div>
 
-            <div className="flex flex-col gap-2  sm:flex-row items-center justify-between text-white text-md sm:text-xs sm:pt-4">
-                <p>Copyright &copy; All right reserved USC 2025</p>
+            <div className="flex flex-col gap-2  sm:flex-row items-center justify-between text-white text-xs sm:pt-4">
+                <p>Copyright &copy; All right reserved F&P 2025</p>
                 <p>Privacy Policy | Terms and Conditions</p>
                 <BackToTop />
             </div>
