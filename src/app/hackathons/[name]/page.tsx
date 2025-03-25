837955616084e35a8b6a4e678ef53f464aef6dda
Florien Niyongabo <waka.florien45@gmail.com>
2025-03-25 23:07:32 +0200

    Add pending button style and key instructions section in SingleHackathon; implement join challenge functionality

--- src/app/hackathons/[name]/page.tsx
+++ src/app/hackathons/[name]/page.tsx
@@ -10,6 +10,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getSingleChallenge } from "@/apis";
 import { Footer } from "@/components/Footer";
 import { Nav } from "@/components/Nav";
+import Button from "@/components/Button";
+import { LuBadgeDollarSign, LuCalendarDays } from "react-icons/lu";
+import { HiOutlineBriefcase } from "react-icons/hi";
+import { IoMailOutline } from "react-icons/io5";
 
 const productDesign = [
     "User Interface Design for each step",
@@ -46,6 +50,32 @@ const SingleHackathon = ({ searchParams }) => {
     // Action Functions
     const selectedChallenge = decodeUrl(pathname.split("/hackathons/")[1])
 
+    const instructions = [
+        {
+            title: singleChallenge && singleChallenge.data.contactEmail,
+            subTitle: "Contact Email",
+            icon: <IoMailOutline className="size-5 text-primary" />
+        },
+        {
+            title: singleChallenge && singleChallenge.data.challengeName,
+            subTitle: "Challenge category",
+            icon: <HiOutlineBriefcase className="size-5 text-primary" />
+        },
+        {
+            title: singleChallenge && singleChallenge.data.duration + " days",
+            subTitle: "Duration",
+            icon: <LuCalendarDays className="size-5 text-primary" />
+        },
+        {
+            title: singleChallenge && singleChallenge.data.moneyPrize,
+            subTitle: "Prize Money",
+            icon: <LuBadgeDollarSign className="size-5 text-primary" />
+        },
+    ]
+    const handleJoinChallenge = () => {
+        console.log("Joining Challenge")
+    }
+
     return (
 
         <div className="bg-backgroundA relative flex flex-col zoom-out">
@@ -69,8 +99,9 @@ const SingleHackathon = ({ searchParams }) => {
                             <span className="text-primary capitalize">{selectedChallenge}</span>
                         </div>
                     </div>
-                    {isLoading ? <p>Loading ...</p> : (<div className='grid sm:grid-cols-3 gap-2 sm:gap-4'>
-                        <div className="col-span-2 bg-white grid sm:space-y-4 space-y-2 sm:p-4 p-2 border rounded-lg">
+
+                    {isLoading ? <p>Loading ...</p> : (<div className='grid grid-cols-1 sm:grid-cols-3 gap-2'>
+                        <div className="col-span-2 bg-white grid sm:space-y-4 space-y-2 sm:p-6 p-4 border rounded-lg">
 
                             <div className="relative bg-primary flex flex-col w-full h-[240px] items-center justify-center rounded-md">
                                 <Image
@@ -92,16 +123,14 @@ const SingleHackathon = ({ searchParams }) => {
                             <div className="sm:space-y-2">
                                 <h1 className="font-bold text-sm sm:text-md">Tasks:</h1>
                                 <h1 className="font-bold text-sm sm:text-md capitalize">Product Requirements</h1>
-                                {/* <ul className="sm:text-md list-disc pl-4">
-                                        {productRequirements.map(item => (<li key={item}>{item}</li>))}
-                                    </ul> */}
+
                                 <p className="sm:text-md capitalize">{singleChallenge && singleChallenge.data.projectTasks}</p>
                             </div>
 
                             {/* Product Design */}
                             <div className="sm:space-y-2">
                                 <h1 className="font-bold text-sm sm:text-md">Product Design</h1>
-                                <ul className="sm:text-md list-disc pl-4">
+                                <ul className="sm:text-md list-disc list-outside  marker:text-gray-500 pl-4">
                                     {productDesign.map(item => (<li key={item}>{item}</li>))}
                                 </ul>
                             </div>
@@ -109,7 +138,7 @@ const SingleHackathon = ({ searchParams }) => {
                             {/* Deliverables */}
                             <div className="sm:space-y-2">
                                 <h1 className="font-bold text-sm sm:text-md">Deliverables</h1>
-                                <ul className="sm:text-md list-disc pl-4">
+                                <ul className="sm:text-md  list-disc list-outside  marker:text-gray-500 pl-4">
                                     {deliverables.map(item => (<li key={item}>{item}</li>))}
                                 </ul>
                             </div>
@@ -117,14 +146,47 @@ const SingleHackathon = ({ searchParams }) => {
                             {/* Notes */}
                             <div className="sm:space-y-2">
                                 <h1 className="font-bold text-sm sm:text-md">Notes</h1>
-                                <ul className="sm:text-md list-disc pl-4">
+                                <ul className="sm:text-md  list-disc list-outside  marker:text-gray-500 pl-4">
                                     <li >Find Product Requirements Summary and Features Description for Saway Pay <a href="#">HERE</a></li>
                                 </ul>
                             </div>
 
                         </div>
+                        <div>
+                            <div className='bg-white h-fit w-full flex flex-col items-start gap-2 sm:gap-4 p-4 sm:p-6 border rounded-lg'>
+                                <header className='space-y-2 sm:space-y-4'>
+                                    <h1 className='font-bold text-sm sm:text-md'>Key Instructions</h1>
+                                    <p className="sm:text-md">You are free to schedule the clarification call with the team via this</p>
+                                </header>
+
+                                {(<div className="flexflex-col items-start space-y-3 sm:space-y-6">
+                                    {instructions.map((item, index) => (<div key={index} className="flex items-center  sm:justify-start gap-2 sm:gap-4">
+                                        <div className='bg-[#D0E0FC] flex items-center justify-center h-10 w-10 sm:p-2 rounded-full cursor-pointer'>
+                                            {item.icon}
+                                        </div>
+                                        <div className="flex sm:flex-col">
+                                            <p className="text-secondary text-sm sm:text-md font-bold">{item.title}</p>
+                                            <p className="text-secondary text-sm sm:text-md"> {item.subTitle}</p>
+                                        </div>
+                                    </div>))}
+                                </div>)}
+
+                                <div className="w-full sm:py-4">
+                                    <div className="w-full flex flex-wrap gap-2 ">
+                                        <Button className={`pending-btn`} onClick={handleJoinChallenge} >Join Challenge</Button>
+
+                                        <Button className={`primary-btn !w-fit`} onClick={() => console.log("Submitted")} > Submit your Work</Button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>)}
                 </div>
+
+
+
+
             </main>
             <Footer />
         </div>
